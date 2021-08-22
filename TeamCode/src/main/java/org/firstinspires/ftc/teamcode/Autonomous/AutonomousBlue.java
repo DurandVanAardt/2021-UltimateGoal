0705e6f38930bb68e098b3c8b90a6111e292837d
Jayden Bailie <Rbailie@mweb.co.za>
2021-08-22 17:32:19 +0200

    25 August 2021

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutonomousBlue.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Initialization.Initialize;
 import org.firstinspires.ftc.teamcode.Initialization.Variables;
 import org.firstinspires.ftc.teamcode.Resources.Motors;
@@ -25,6 +26,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
 
 import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
@@ -42,6 +44,7 @@ public class AutonomousBlue extends LinearOpMode {
     private boolean turning = false;
     boolean turningRight = false;
     boolean TurnRight = false;
+    boolean bBoolean = true;
 
 
     Variables var;
@@ -106,192 +109,128 @@ public class AutonomousBlue extends LinearOpMode {
 
         webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
 
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
-
-        // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-
-        /**
-         * We also indicate which camera on the RC we wish to use.
-         */
-        parameters.cameraName = webcamName;
-
-        // Make sure extended tracking is disabled for this example.
-        parameters.useExtendedTracking = false;
-
-        //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-        // Load the data sets for the trackable objects. These particular data
-        // sets are stored in the 'assets' part of our application.
-        VuforiaTrackables targetsUltimateGoal = this.vuforia.loadTrackablesFromAsset("UltimateGoal");
-        VuforiaTrackable blueTowerGoalTarget = targetsUltimateGoal.get(0);
-        blueTowerGoalTarget.setName("Blue Tower Goal Target");
-        VuforiaTrackable redTowerGoalTarget = targetsUltimateGoal.get(1);
-        redTowerGoalTarget.setName("Red Tower Goal Target");
-        VuforiaTrackable redAllianceTarget = targetsUltimateGoal.get(2);
-        redAllianceTarget.setName("Red Alliance Target");
-        VuforiaTrackable blueAllianceTarget = targetsUltimateGoal.get(3);
-        blueAllianceTarget.setName("Blue Alliance Target");
-        VuforiaTrackable frontWallTarget = targetsUltimateGoal.get(4);
-        frontWallTarget.setName("Front Wall Target");
-
-        // For convenience, gather together all the trackable objects in one easily-iterable collection */
-        List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
-        allTrackables.addAll(targetsUltimateGoal);
-
-        /**
-         * In order for localization to work, we need to tell the system where each target is on the field, and
-         * where the phone resides on the robot.  These specifications are in the form of <em>transformation matrices.</em>
-         * Transformation matrices are a central, important concept in the math here involved in localization.
-         * See <a href="https://en.wikipedia.org/wiki/Transformation_matrix">Transformation Matrix</a>
-         * for detailed information. Commonly, you'll encounter transformation matrices as instances
-         * of the {@link OpenGLMatrix} class.
-         *
-         * If you are standing in the Red Alliance Station looking towards the center of the field,
-         *     - The X axis runs from your left to the right. (positive from the center to the right)
-         *     - The Y axis runs from the Red Alliance Station towards the other side of the field
-         *       where the Blue Alliance Station is. (Positive is from the center, towards the BlueAlliance station)
-         *     - The Z axis runs from the floor, upwards towards the ceiling.  (Positive is above the floor)
-         *
-         * Before being transformed, each target image is conceptually located at the origin of the field's
-         *  coordinate system (the center of the field), facing up.
-         */
-
-        //Set the position of the perimeter targets with relation to origin (center of field)
-        redAllianceTarget.setLocation(OpenGLMatrix
-                .translation(0, -halfField, mmTargetHeight)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 180)));
-
-        blueAllianceTarget.setLocation(OpenGLMatrix
-                .translation(0, halfField, mmTargetHeight)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 0)));
-        frontWallTarget.setLocation(OpenGLMatrix
-                .translation(-halfField, 0, mmTargetHeight)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , 90)));
-
-        // The tower goal targets are located a quarter field length from the ends of the back perimeter wall.
-        blueTowerGoalTarget.setLocation(OpenGLMatrix
-                .translation(halfField, quadField, mmTargetHeight)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , -90)));
-        redTowerGoalTarget.setLocation(OpenGLMatrix
-                .translation(halfField, -quadField, mmTargetHeight)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90)));
-
-        if (CAMERA_CHOICE == BACK) {
-            phoneYRotate = -90;
-        } else {
-            phoneYRotate = 90;
-        }
-
-        // Rotate the phone vertical about the X axis if it's in portrait mode
-        if (PHONE_IS_PORTRAIT) {
-            phoneXRotate = 90 ;
-        }
-
-        // Next, translate the camera lens to where it is on the robot.
-        // In this example, it is centered (left to right), but forward of the middle of the robot, and above ground level.
-        final float CAMERA_FORWARD_DISPLACEMENT  = 4.0f * mmPerInch;   // eg: Camera is 4 Inches in front of robot-center
-        final float CAMERA_VERTICAL_DISPLACEMENT = 8.0f * mmPerInch;   // eg: Camera is 8 Inches above ground
-        final float CAMERA_LEFT_DISPLACEMENT     = 0;     // eg: Camera is ON the robot's center line
-
-        OpenGLMatrix robotFromCamera = OpenGLMatrix
-                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
-                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, YZX, DEGREES, phoneYRotate, phoneZRotate, phoneXRotate));
-
-        /**  Let all the trackable listeners know where the phone is.  */
-        for (VuforiaTrackable trackable : allTrackables) {
-            ((VuforiaTrackableDefaultListener) trackable.getListener()).setPhoneInformation(robotFromCamera, parameters.cameraDirection);
-        }
-
-
-
-        targetsUltimateGoal.activate();
-        while (!isStopRequested()) {
-
-            // check all the trackable targets to see which one (if any) is visible.
-            targetVisible = false;
-            for (VuforiaTrackable trackable : allTrackables) {
-
-                if (((VuforiaTrackableDefaultListener)trackable.getListener()).isVisible()) {
-                    telemetry.addData("Visible Target", trackable.getName());
-                    targetVisible = true;
-
-                    // getUpdatedRobotLocation() will return null if no new information is available since
-                    // the last time that call was made, or if the trackable is not currently visible.
-                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener)trackable.getListener()).getUpdatedRobotLocation();
-                    if (robotLocationTransform != null) {
-                        lastLocation = robotLocationTransform;
-                    }
-                    break;
-                }
-            }
-
-            // Provide feedback as to where the robot is located (if we know).
-            if (targetVisible) {
-                // express position (translation) of robot in inches.
-                VectorF translation = lastLocation.getTranslation();
-                telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
-
-                // express the rotation of the robot in degrees.
-                Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
-                telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
-            }
-            else {
-                telemetry.addData("Visible Target", "none");
-            }
-            telemetry.update();
-        }
-
         waitForStart();
 
+//        ShooterState(AutonomousShooter.AUTOMATEDSHOOTERREST);
+
+//
+//    ShooterState(AutonomousShooter.AUTOMATEDSHOOTER);
+
+//        while (opModeIsActive()) {
+//            robot.shooterMotor.setPower(-1);
+//
+//        }
+
+       //shooting
+        while (robot.distanceR.getDistance(DistanceUnit.MM) >450)
+        {
+            motors.driveStrafe(135 * Math.PI / 180, 0.4, true);
+
+        }
+        motors.stop();
+        telemetry.addData("Right",robot.distanceR.getDistance(DistanceUnit.MM));
+
+sleep(3000);
+        while (robot.distanceB.getDistance(DistanceUnit.MM) < 450)
+        {
+            robot.leftFront.setPower(0.1);
+            robot.leftBack.setPower(0.1);
+            robot.rightFront.setPower(0.1);
+            robot.rightBack.setPower(0.1);
+        }
+        motors.stop();
+
+        robot.shooterMotor.setPower(-1);
+        robot.magazineLifter.setPosition(0);
+        sleep(700);
+        robot.Tap.setPosition(0);
+        sleep(500);
+        robot.Tap.setPosition(0.4);
+        sleep(500);
+        robot.Tap.setPosition(0);
+        sleep(500);
+        robot.Tap.setPosition(0.4);
+        robot.magazineLifter.setPosition(0.4);
+        sleep(700);
+        robot.sucker.setPower(-1);
+        sleep(2000);
+        robot.sucker.setPower(0);
+        robot.magazineLifter.setPosition(0);
+        sleep(700);
+        robot.Tap.setPosition(0);
+
+//        ShooterState(AutonomousShooter.TAPRIGHT);
+//
+//        ShooterState(AutonomousShooter.TAPDEFAULT);
 
 
+
+
+
+//        while (robot.Tap.getPosition() != 0.4) {
+//        ShooterState(AutonomousShooter.TAPDEFAULT);
+//    }
+//
+//        while (robot.Tap.getPosition() != 0) {
+//            ShooterState(AutonomousShooter.TAPRIGHT);
+//        }
+
+
+//        while (robot.Tap.getPosition() != 0.4) {
+//            ShooterState(AutonomousShooter.TAPDEFAULT);
+//        }
 // ----Turning----//
 
-        MoveState(DriveTrain.TURNLEFT); // Turn to 90 left
-
-        MoveState(DriveTrain.TURNFRONT); // Turn to start position
+//        MoveState(DriveTrain.TURNLEFT); // Turn to 90 left
+//
+//        MoveState(DriveTrain.TURNFRONT); // Turn to start position
 
 // ----Shooter on ---- //
 
-        motors.Fire(10000);
+//        motors.Fire(10000);
+//////
+//        while (robot.shooterMotor.getCurrentPosition() < robot.shooterMotor.getTargetPosition()) {
+//            robot.shooterMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//            robot.shooterMotor.setPower(-1);
+////}
 
-while (robot.shooterMotor.getCurrentPosition() < robot.shooterMotor.getTargetPosition()) {
-    robot.shooterMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-    robot.shooterMotor.setPower(1);
-}
+            //----Driving forward---- //
 
- //----Driving forward---- //
-
-        motors.driveStrafe(45 * Math.PI / 180, 1, true);
-        while(!motors.pidStrafe.onTarget())
-
+//            motors.stop();
+//while  (bBoolean)
+//        {
+//            robot.leftFront.setPower(0.5);
+//            robot.rightBack.setPower(0.5);
+//            robot.rightFront.setPower(0.5);
+//            robot.leftBack.setPower(0.5);
+//        }
 //
+
+
 //
 //        if ((robot.distanceL.getDistance(DistanceUnit.MM) < 300))
 //            motors.stop();
-
 
 
 // ----Strafe Right---- //
-
-        while(!motors.pidStrafe.onTarget())
-            motors.driveStrafe(135 * Math.PI / 180, 1, true);
+//while (bBoolean)
+//        motors.driveStrafe(135 * Math.PI / 180, 0.4, true);
 
 //        if ((robot.distanceL.getDistance(DistanceUnit.MM) < 300))
 //            motors.stop();
 
+//----Strafe Left-----
+//while (bBoolean)
+//        motors.driveStrafe(-45 * Math.PI / 180, 0.2, true);
 
-        telemetry.addData("IMU", var.getAngle());
-        telemetry.update();
+
+            telemetry.addData("IMU", var.getAngle());
+            telemetry.update();
+
+
 
 
     }
-
     /**
      * Returns an estimation of the horizontal angle to the detected object.
      */
@@ -349,11 +288,31 @@ while (robot.shooterMotor.getCurrentPosition() < robot.shooterMotor.getTargetPos
 
             switch (Shooter) {
                 case FIRE:
-
+                    robot.shooterMotor.setPower(-1);
                 break;
+
+                case TAPDEFAULT:
+                    robot.Tap.setPosition(0.4);
+break;
+
+                case TAPRIGHT:
+                    robot.Tap.setPosition(0);
+                    break;
+
+
+                case AUTOMATEDSHOOTER:
+
+//                    robot.Tap.setPosition(0.4);
+break;
+
+                case AUTOMATEDSHOOTERREST:
+                    robot.shooterMotor.setPower(0);
+                    robot.magazineLifter.setPosition(0);
+                    robot.Tap.setPosition(0);
             }
 
         }
+
 
 
 
